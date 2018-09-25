e29dcf7c32c13a4c885af360ac3b3d53416606a5
Vollodymyr K <your.easy.choice@gmail.com>
2018-09-26 00:30:39 +0300

    Lesson 19

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 knp
 
+https://coursehunters.net/course/knp-symfony3
+
+Lesson 19. .config_dev_yml & config_prod_yml
+
 https://www.free-css.com/free-css-templates/page210/piccolo
--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -15,8 +15,21 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
+        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $key = md5($funFact);
+
+        if ($cache->contains($key)) {
+            $funFact = $cache->fetch($key);
+        } else {
+            $funFact = $this->get('markdown.parser')->transform($funFact);
+            $cache->save($key, $funFact);
+        }
+
         return $this->render('genus/show.html.twig', [
             'name' => $genusName,
+            'funFact' => $funFact
         ]);
     }
 
