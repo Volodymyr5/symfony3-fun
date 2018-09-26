4774bcb3f8a7e4e61c4ad7bdc723231a2d99b5fa
Vollodymyr K <your.easy.choice@gmail.com>
2018-09-27 02:56:06 +0300

    Lesson 28

--- README.md
+++ README.md
@@ -2,6 +2,4 @@ knp
 
 https://coursehunters.net/course/knp-symfony3
 
-Lesson 19. .config_dev_yml & config_prod_yml
-
-https://www.free-css.com/free-css-templates/page210/piccolo
+Lesson 28.
---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Genus
+ * @package AppBundle\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $subFamily;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $speciesCount;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $funFact;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubFamily()
+    {
+        return $this->subFamily;
+    }
+
+    /**
+     * @param mixed $subFamily
+     */
+    public function setSubFamily($subFamily)
+    {
+        $this->subFamily = $subFamily;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSpeciesCount()
+    {
+        return $this->speciesCount;
+    }
+
+    /**
+     * @param mixed $speciesCount
+     */
+    public function setSpeciesCount($speciesCount)
+    {
+        $this->speciesCount = $speciesCount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFunFact()
+    {
+        return $this->funFact;
+    }
+
+    /**
+     * @param mixed $funFact
+     */
+    public function setFunFact($funFact)
+    {
+        $this->funFact = $funFact;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        // TODO 
+        return 0;//\DateTime('-', rand(1, 100) . ' days');
+    }
+}
